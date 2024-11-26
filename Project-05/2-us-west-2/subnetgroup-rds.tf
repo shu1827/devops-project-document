cdf98f306b12d237a6e3123fd6b78e52563c30f7
shubham <shubhamshetkar46@gmail.com>
2024-11-26 23:53:46 +0530

    updated

--- Project-05/2-us-west-2/subnetgroup-rds.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_db_subnet_group" "sub-grp" {
-  name       = "main"
-  subnet_ids = [aws_subnet.prvt7.id, aws_subnet.prvt8.id]
-  depends_on = [ aws_subnet.prvt7,aws_subnet.prvt8 ]
-
-  tags = {
-    Name = "My-DB-subnet-group"
-  }
-}
--- Project-05/3-us-east-1/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {  
-  region = "us-east-1"
-  }
-provider "aws" {
-  alias  = "secondary"  
-  region = "us-west-2"
-}
